ffafecea2f5ad91bba855209db9c551998104006
Kaan Dinler <kaan_d_tr@hotmail.com>
2023-02-28 20:58:43 +0300

    new main
    (cherry picked from commit 3d15aeb27ea7678bad078ba71d4d6623440bbdce)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_syncfusion/stack_area_graph_ex.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+
+import 'my_spline_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,21 +37,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late TooltipBehavior _tooltipBehavior;
-
-  List<SalesData> myDataSource = <SalesData>[
-    SalesData('09:00', 11000),
-    SalesData('10:00', 14000),
-    SalesData('11:00', 23000),
-    SalesData('12:00', 28000),
-    SalesData('13:00', 35000),
-    SalesData('14:00', 22000)
-  ];
+  ChartSeriesController? seriesController;
+  var random = Random();
+  late List<ChartData> _chartData;
 
   @override
   void initState() {
-    _tooltipBehavior = TooltipBehavior(enable: true);
     super.initState();
+    _chartData = <ChartData>[
+      ChartData('Energy', 183),
+      ChartData('Finances', 300),
+      ChartData('Entertaintment', 125),
+      ChartData('Consumer Electric', 135),
+      ChartData('Healtcare', 62),
+      ChartData('Technology', 502),
+    ];
   }
 
   @override
@@ -56,81 +61,53 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.height * 0.4,
+        child: Container(
+          color: Color(0xFF141416),
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width * 0.9,
           child: SfCartesianChart(
-            crosshairBehavior: CrosshairBehavior(
-              enable: true,
-              activationMode: ActivationMode.longPress,
-              shouldAlwaysShow: false,
-              lineType: CrosshairLineType.vertical,
-              lineColor: Colors.grey.withOpacity(0.3),
-              lineWidth: 1,
-            ),
             plotAreaBorderWidth: 0,
-            borderWidth: 0,
-            borderColor: Colors.transparent,
-            backgroundColor: const Color(0Xff222225),
             primaryXAxis: CategoryAxis(
-                majorGridLines: const MajorGridLines(width: 0),
-                majorTickLines: const MajorTickLines(size: 0),
-                minorTickLines: const MinorTickLines(size: 0),
-                axisLine: const AxisLine(width: 0),
-                borderWidth: 0,
-                borderColor: Colors.transparent,
-                axisBorderType: AxisBorderType.withoutTopAndBottom,
-                interval: 1),
-            primaryYAxis: NumericAxis(
-              borderWidth: 0,
-              borderColor: Colors.transparent,
-              majorGridLines: MajorGridLines(width: 1, color: Colors.grey.withOpacity(0.3)),
+              axisLine: const AxisLine(width: 2),
               majorTickLines: const MajorTickLines(size: 0),
-              minorTickLines: const MinorTickLines(size: 0),
-              axisLine: const AxisLine(width: 0),
-              interval: 10000,
-              // minimum: myDataSource.reduce((value, element) => value.growth < element.growth ? value : element).growth,
-              minimum: 10000,
+              majorGridLines: const MajorGridLines(width: 0),
+              labelPosition: ChartDataLabelPosition.outside,
+              labelAlignment: LabelAlignment.center,
+              labelStyle: const TextStyle(color: Colors.white, fontSize: 12),
+              labelIntersectAction: AxisLabelIntersectAction.trim,
+              labelPlacement: LabelPlacement.betweenTicks,
+              edgeLabelPlacement: EdgeLabelPlacement.shift,
             ),
-            trackballBehavior: TrackballBehavior(
-              shouldAlwaysShow: false,
-              enable: true,
-              activationMode: ActivationMode.singleTap,
-              lineWidth: 1,
-              lineDashArray: const <double>[5, 5],
-              lineColor: Color(0xFF23D4BF),
-              markerSettings: const TrackballMarkerSettings(
-                markerVisibility: TrackballVisibilityMode.visible,
-                shape: DataMarkerType.circle,
-                height: 11,
-                width: 11,
-                borderWidth: 7,
-                borderColor: Colors.white,
-                color: Color(
-                  0xFF23D4BF,
-                ),
-              ),
-              tooltipSettings: const InteractiveTooltip(
-                arrowLength: 8,
-                borderRadius: 3,
-                canShowMarker: false,
-                format: 'point.y\nMay',
-                enable: true,
+            primaryYAxis: NumericAxis(
+              labelFormat: ' ',
+              axisLine: const AxisLine(width: 2),
+              majorTickLines: const MajorTickLines(size: 0),
+              majorGridLines: const MajorGridLines(width: 0),
+              tickPosition: TickPosition.outside,
+            ),
+            series: <StackedColumnSeries<ChartData, String>>[
+              StackedColumnSeries<ChartData, String>(
+                dataSource: _chartData,
+                xValueMapper: (ChartData data, _) => data.month,
+                yValueMapper: (ChartData data, _) => data.value,
                 color: Color(0xFF2D2D33),
-                borderColor: Colors.black,
-                borderWidth: 0,
-                textStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            series: <SplineSeries>[
-              SplineSeries<SalesData, String>(
-                color: Color(0xFF23D4BF),
-                splineType: SplineType.cardinal,
-                enableTooltip: true,
-                width: 5,
-                dataSource: myDataSource,
-                xValueMapper: (SalesData sales, _) => sales.time,
-                yValueMapper: (SalesData sales, _) => sales.growth,
+                isTrackVisible: true,
+                trackColor: Color(0xFF222225),
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                //Sorts the data points in descending order
+                sortingOrder: SortingOrder.descending,
+                sortFieldValueMapper: (ChartData data, _) => data.value,
+                //------------------------------
+                legendIconType: LegendIconType.rectangle,
+                spacing: 0.1,
+                dataLabelSettings: DataLabelSettings(
+                  isVisible: true,
+                  labelAlignment: ChartDataLabelAlignment.middle,
+                  labelPosition: ChartDataLabelPosition.inside,
+                  textStyle: const TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w700),
+                  
+                ),
+                dataLabelMapper: (ChartData data, _) => data.value.toString(),
               ),
             ],
           ),
@@ -140,9 +117,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class SalesData {
-  final String time;
-  final double growth;
+class ChartData {
+  final String month;
+  final int value;
 
-  SalesData(this.time, this.growth);
+  ChartData(
+    this.month,
+    this.value,
+  );
 }
